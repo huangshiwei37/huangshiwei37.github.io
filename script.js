de82ab535bb004df26862624c6dc0b06dded2aa3
Shiwei Huang <huangshiwei37@gmail.com>
2018-05-08 01:29:46 -0500

    Submit My Homepage V1.0
    The first version of my homepage, named Version 1.0, is complete.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// $('#myCarousel').carousel();
+
+// $('carousel-control-prev').click(function() {
+//   $('#myCarousel').carousel('prev');
+// });
+
+// $('carousel-control-next').click(function() {
+//   $('#myCarousel').carousel('next');
+// });
